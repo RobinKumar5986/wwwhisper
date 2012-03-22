6799315fa2107be04f476c472cbb88ace701037f
Jan Wrobel <wrr@mixedbit.org>
2012-03-22 17:42:40 +0100

    assertion sumbition and redirection

--- src/auth_static/login.js
+++ src/auth_static/login.js
@@ -8,9 +8,39 @@
     );
   }
 
-  $(document).ready(function() {
-    $('.site').text(location.host + getURLParameter('next'));
-    $('.hidden').removeClass('hidden');
-  });
 
+  // http://stackoverflow.com/questions/3846271/
+  // jquery-submit-post-synchronously-not-ajax
+  function sendAssertion(assertion) {
+    if (assertion) {
+      $('body').append($('<form/>', {
+        id: 'hidden-form',
+        method: 'POST',
+        action: 'api/browserid/verify/',
+      }));
+
+      $('#hidden-form').append($('<input/>', {
+        type: 'hidden',
+        name: 'assertion',
+        value: assertion.toString()
+      }));
+
+      $('#hidden-form').append($('<input/>', {
+        type: 'hidden',
+        name: 'next',
+        value: getURLParameter('next')
+      }));
+
+      $('#hidden-form').submit();
+    } else {
+      alert('BrowserID assertion not set');
+    }
+  }
+
+  $('.site').text(location.host + getURLParameter('next'));
+  $('.hidden').removeClass('hidden');
+  $('#browserid').click(function() {
+    navigator.id.get(sendAssertion);
+    return false;
+  });
 }());
