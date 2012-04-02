b3cf4e425ab047748932f7a40b35a8a5175c8b0f
Jan Wrobel <wrr@mixedbit.org>
2012-04-02 16:54:31 +0200

    API refactoring + tests

--- django/wwwhisper_auth/acl.py
+++ django/wwwhisper_auth/acl.py
@@ -1,12 +1,17 @@
+from django.contrib.auth.models import User
 from wwwhisper_auth.models import HttpResource
 from wwwhisper_auth.models import HttpPermission
 
+# def _add(model_class, **kwargs):
+#     resource, created = model_class.objects.get_or_create(**kwargs)
+#     return created
+
 def add_resource(path):
-    resource, created = HttpResource.objects.get_or_create(path = path)
+    resource, created = HttpResource.objects.get_or_create(path=path)
     return created
 
 def del_resource(path):
-    resource = HttpResource.objects.filter(path = path)
+    resource = HttpResource.objects.filter(path=path)
     assert resource.count() <= 1
     if resource.count() == 0:
         return False
@@ -14,9 +19,33 @@ def del_resource(path):
     return True
 
 def find_resource(path):
-    count = HttpResource.objects.filter(path = path).count()
+    count = HttpResource.objects.filter(path=path).count()
     assert count <= 1
     return count > 0
 
-def resources():
+def paths():
     return [resource.path for resource in HttpResource.objects.all()]
+
+
+def add_user(email):
+    user, created = User.objects.get_or_create(
+        username=email, email=email, is_active=True)
+    return created
+
+def del_user(email):
+    user = User.objects.filter(email=email)
+    assert user.count() <= 1
+    if user.count() == 0:
+        return False
+    user.delete()
+    return True
+
+def find_user(email):
+    count = User.objects.filter(email=email).count()
+    assert count <= 1
+    return count > 0
+
+def emails():
+    return [user.email for user in User.objects.all()]
+
+
--- django/wwwhisper_auth/tests.py
+++ django/wwwhisper_auth/tests.py
@@ -22,9 +22,37 @@ class Acl(TestCase):
     def test_get_paths(self):
         acl.add_resource('/foo/bar')
         acl.add_resource('/baz/bar')
-        self.assertEqual(sorted(acl.paths()), ['/baz/bar', '/foo/bar'])
+        self.assertEqual(['/baz/bar', '/foo/bar'], sorted(acl.paths()))
         acl.del_resource('/foo/bar')
-        self.assertEqual(acl.paths(), ['/baz/bar'])
+        self.assertEqual(['/baz/bar'], acl.paths())
 
     def test_get_paths_when_empty(self):
-        self.assertEqual(acl.paths(), [])
+        self.assertEqual([], acl.paths())
+
+    def test_add_user(self):
+        self.assertFalse(acl.find_user('foo@example.com'))
+        self.assertTrue(acl.add_user('foo@example.com'))
+        self.assertTrue(acl.find_user('foo@example.com'))
+
+    def test_add_user_twice(self):
+        self.assertTrue(acl.add_user('foo@example.com'))
+        self.assertFalse(acl.add_user('foo@example.com'))
+
+    def test_del_user(self):
+        self.assertTrue(acl.add_user('foo@example.com'))
+        self.assertTrue(acl.del_user('foo@example.com'))
+        self.assertFalse(acl.find_user('foo@example.com'))
+
+    def test_del_missing_user(self):
+        self.assertFalse(acl.del_user('foo@example.com'))
+
+    def test_get_emails(self):
+        acl.add_user('foo@example.com')
+        acl.add_user('bar@example.com')
+        self.assertEqual(['bar@example.com', 'foo@example.com'],
+                         sorted(acl.emails()))
+        acl.del_user('foo@example.com')
+        self.assertEqual(['bar@example.com'], acl.emails())
+
+    def test_get_emails_when_empty(self):
+        self.assertEqual([], acl.emails())
