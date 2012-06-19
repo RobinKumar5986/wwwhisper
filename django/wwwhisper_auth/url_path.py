2c69c6bb58d0a7fabcbe5e98a052611b5ee6bc01
Jan Wrobel <wrr@mixedbit.org>
2012-06-19 23:31:07 +0200

    Comments added and fixed lint warnings.

--- django/wwwhisper_auth/url_path.py
+++ django/wwwhisper_auth/url_path.py
@@ -1,20 +1,30 @@
+"""Functions that operate on an HTTP resource path."""
+
 import posixpath
 import urllib
 
 def strip_query(path):
+    """Strips query from a path."""
     query_start = path.find('?')
     if query_start != -1:
         return path[:query_start]
     return path
 
 def decode(path):
+    """Decodes URL encoded characters in path."""
     return urllib.unquote_plus(path)
 
 def is_canonical(path):
+    """True if path is absolute and normalized.
+
+    Canonical path is unique, i.e. two different such paths are never
+    equivalent.
+    """
     # Posix recognizes '//' as a normalized path, but it is not
     # canonical (it is the same as '/').
     if path == '' or not posixpath.isabs(path) or path.startswith('//'):
         return False
+    # Normpath remove trailing '/'.
     normalized_path =  posixpath.normpath(path)
     if (normalized_path != path and normalized_path + '/' != path):
         return False
@@ -22,10 +32,13 @@ def is_canonical(path):
 
 
 def contains_fragment(path):
+    """True if path contains fragment id ('#' part)."""
     return path.count('#') != 0
 
 def contains_query(path):
+    """True if path contains query string ('?' part)."""
     return path.count('?') != 0
 
 def contains_params(path):
+    """True if path contains params (';' part)."""
     return path.count(';') != 0
--- django/wwwhisper_auth/urls.py
+++ django/wwwhisper_auth/urls.py
@@ -1,8 +1,12 @@
-from django.conf.urls.defaults import patterns, url
-from views import CsrfToken, Auth, Login, Logout
-from wwwhisper_auth.models import LocationsCollection
+"""Urls exposed by the wwwhisper_auth app.
 
-locations_collection = LocationsCollection()
+is-authorized/ URL does not need to be exposed by the HTTP server to
+the outside world, other views need to be externally accessible.
+"""
+
+from django.conf.urls.defaults import patterns, url
+from views import Auth, CsrfToken, Login, Logout
+from wwwhisper_auth.models import LocationsCollection
 
 urlpatterns = patterns(
     'wwwhisper_auth.views',
@@ -10,5 +14,5 @@ urlpatterns = patterns(
     url(r'^login/$', Login.as_view()),
     url(r'^logout/$', Logout.as_view()),
     url(r'^is-authorized/$', Auth.as_view(
-            locations_collection=locations_collection)),
+            locations_collection=LocationsCollection())),
     )
