e71849d0987ae64618bc6f17dcc60146b1c189d4
Jan Wrobel <wrr@mixedbit.org>
2012-06-20 22:51:53 +0200

    Comments added.

--- django/wwwhisper_auth/backend.py
+++ django/wwwhisper_auth/backend.py
@@ -1,20 +1,35 @@
-"""backend that verifies BrowserID assertion.
+"""Authentication backend used by wwwhisper_auth."""
 
-Very similar backend is defined in django_browserid application. It is
-not used here, because it does not allow to distinguish between an
-assertion verification error and an unknown user.
-"""
 from django.contrib.auth.backends import ModelBackend
 from django_browserid.base import verify
 from wwwhisper_auth import models
 
 class AssertionVerificationException(Exception):
-    pass;
+    """Raised when BrowserId assertion was not verified successfully."""
+    pass
 
 class BrowserIDBackend(ModelBackend):
-    users_collection = models.UsersCollection();
+    """"Backend that verifies BrowserID assertion.
+
+    Similar backend is defined in django_browserid application. It is not
+    used here, because it does not allow to distinguish between an
+    assertion verification error and an unknown user.
+
+    Attributes:
+        users_collection: Allows to find a user with a given email.
+    """
+    users_collection = models.UsersCollection()
 
     def authenticate(self, assertion):
+        """Verifies BrowserID assertion
+
+        Returns:
+             Object that represents a user with an email verified by
+             the assertion. None if user with such email does not exist.
+
+        Raises:
+            AssertionVerificationException: verification failed.
+        """
         result = verify(assertion=assertion, audience=models.SITE_URL)
         if result is None:
             raise AssertionVerificationException(
