e039cbf5d5591c25639f40464ef918783d31708a
Jan Wrobel <wrr@mixedbit.org>
2012-06-28 17:30:54 +0200

    Copyright statements for Python code.

--- auth_static/overlay.js
+++ auth_static/overlay.js
@@ -15,6 +15,8 @@
               var email;
               email = result.email;
               if (email.length > MAX_EMAIL_LENGTH) {
+                // Trim very long emails so 'sign out' button fits in
+                // the iframe.
                 email = email.substr(0, MAX_EMAIL_LENGTH) + '[...]';
               }
               $('#email').text(email)
