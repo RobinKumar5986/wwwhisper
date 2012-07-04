4c2466afca2090b98fe8713788ffd136180b5aad
Jan Wrobel <wrr@mixedbit.org>
2012-07-04 20:23:38 +0200

    Comments added.

--- auth_static/assets/js/stub.js
+++ auth_static/assets/js/stub.js
@@ -8,9 +8,19 @@
 (function () {
   'use strict';
 
+  /**
+   * Communicates with the server. Makes sure each request carries
+   * cross site request forgery protection cookie. Serializes requests
+   * parameters to JSON.
+   */
   function Stub() {
     var csrfToken = null, errorHandler = null, that = this;
 
+    /**
+     * Helper private function, in addition to all arguments accepted
+     * by a public ajax() function, takes dictionary of headers to be
+     * sent along with the request.
+     */
     function ajaxCommon(method, resource, params, headersDict,
                         successCallback) {
       if (errorHandler !== null) {
@@ -37,7 +47,7 @@
           url: resource,
           type: method,
           data: JSON.stringify(params),
-          contentType: 'application/json',
+          contentType: 'application/json; charset=utf-8',
           headers: headersDict,
           success: successCallback,
           error: function(jqXHR) {
@@ -52,6 +62,10 @@
       }
     }
 
+    /**
+     * Private function that retrieves csrf protection token and
+     * invokes a callback on success.
+     */
     function getCsrfToken(nextCallback) {
       ajaxCommon('POST', '/auth/api/csrftoken/', null, null,
                  function(result) {
@@ -60,9 +74,19 @@
                  });
     }
 
+    /**
+      * Invokes a given HTTP method (a string) on a given resource (a
+      * string), passing to it parameters (an object that the function
+      * serializes to json and that can be null). When successfully
+      * done, invokes a successCallback. On failure, if an error
+      * handler is set, invokes it and passes error message along with
+      * HTTP error code.
+      */
     this.ajax = function(method, resource, params, successCallback) {
       if (csrfToken === null) {
-        // Get token and reexecute the call.
+        // Each HTTP method call, except the one that returns a csrf
+        // protection token needs to carry the token. Get the token
+        // and reexecute the call.
         getCsrfToken(function() {
           that.ajax(method, resource, params, successCallback);
         });
@@ -72,6 +96,9 @@
                  successCallback);
     };
 
+    /**
+     * Sets an error handler that will be invoked when ajax call fails.
+     */
     this.setErrorHandler = function(handler) {
       errorHandler = handler;
     };
