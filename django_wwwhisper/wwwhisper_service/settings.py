e7a9ae127c791f91c6b968bebce35a55893d19d2
Jan Wrobel <wrr@mixedbit.org>
2012-07-12 10:51:15 +0200

    Remove unused setttings.

--- django_wwwhisper/wwwhisper_service/settings.py
+++ django_wwwhisper/wwwhisper_service/settings.py
@@ -3,20 +3,7 @@
 from site_settings import *
 
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
-
-MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'wwwhisper_db',
-    }
-}
+#TEMPLATE_DEBUG = DEBUG
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -35,73 +22,31 @@ SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = True
+USE_I18N = False
 
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale.
-USE_L10N = True
+USE_L10N = False
 
 # If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
+USE_TZ = False
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
+    # TODO: uncomment the next line for simple clickjacking protection
+    # (here or in nginx?):
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-CSRF_COOKIE_NAME = 'wwwhisper-csrftoken'
-# To avoid collision with apps running
+# Don't use just sessionid, to avoid collision with apps protected by wwwhisper.
 SESSION_COOKIE_NAME = 'wwwhisper-sessionid'
-# Make session cookie valid for a one day.
-#SESSION_COOKIE_AGE = 86400
-# Or until a browser close.
-#SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+CSRF_COOKIE_NAME = 'wwwhisper-csrftoken'
+
+# Make session cookie valid until a browser closes.
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
 # TODO: change this:
 SESSION_COOKIE_SECURE = False
 SESSION_COOKIE_HTTPONLY = True
@@ -113,23 +58,26 @@ WSGI_APPLICATION = 'wwwhisper_service.wsgi.application'
 
 INSTALLED_APPS = (
     'django.contrib.auth',
-    'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
     'django_browserid',
     'wwwhisper_auth',
     'wwwhisper_admin'
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
 )
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'wwwhisper_auth.backend.BrowserIDBackend',
+)
+
+
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user': lambda u: "/admin/api/users/%s/" % u.username,
+}
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
+# TODO: make this email working?
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
@@ -156,20 +104,6 @@ LOGGING = {
     }
 }
 
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
-    'wwwhisper_auth.backend.BrowserIDBackend',
-)
-
-SITE_URL = 'http://localhost:8080'
-WWWHISPER_ADMINS = ['wrr@mixedbit.org']
-
-#BROWSERID_CREATE_USER = False
-
-ABSOLUTE_URL_OVERRIDES = {
-    'auth.user': lambda u: "/admin/api/users/%s/" % u.username,
-}
-
 if DEBUG:
     LOGGING = {
         'version': 1,
@@ -186,8 +120,6 @@ if DEBUG:
             'console':{
                 'level':'DEBUG',
                 'class':'logging.StreamHandler',
-                # 'class':'logging.FileHandler',
-                # 'filename': 'debug.log',
                 'formatter': 'simple'
                 },
             },
@@ -207,11 +139,6 @@ if DEBUG:
                 'propagate': True,
                 'level':'DEBUG',
                 },
-            # '': {
-            #     'handlers':['console'],
-            #     'propagate': True,
-            #     'level':'DEBUG',
-            # },
             'django.request': {
                 'handlers': ['console'],
                 'propagate': True,
