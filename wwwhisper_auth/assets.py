361edf51ee680dd8dc55c6336048bf40a4f07403
Jan Wrobel <wrr@mixedbit.org>
2013-01-15 13:41:51 +0100

    Add missing file.

---
+++ wwwhisper_auth/assets.py
@@ -0,0 +1,24 @@
+# wwwhisper - web access control.
+# Copyright (C) 2013 Jan Wrobel <wrr@mixedbit.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+class Asset:
+    """Stores a static file to be returned by requests."""
+
+    def __init__(self, prefix, *args):
+        assert prefix is not None
+        self.body = file(os.path.join(prefix, *args)).read()
