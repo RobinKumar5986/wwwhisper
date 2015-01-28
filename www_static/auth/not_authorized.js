a9ab5288516d8ef8da1c8aebaa10cef65d72bcdb
Jan Wrobel <wrr@mixedbit.org>
2015-01-28 15:21:26 +0100

    Switch to MIT license.
    Closes #15

--- www_static/auth/not_authorized.js
+++ www_static/auth/not_authorized.js
@@ -1,9 +1,5 @@
 /*!
  * wwwhisper - web access control.
- * Copyright (C) 2013 Jan Wrobel
- *
- * Licensed under the GPL License version 3 or any later version:
- * https://www.gnu.org/licenses/gpl-3.0.html
  */
 (function () {
   'use strict';
